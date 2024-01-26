37ad9f5b1564d856b8f5f61c242d7701e93e9741
namhp <haphuongnam81999@gmail.com>
2024-01-26 16:14:22 +0700

    feat sync department and user

---
+++ src/Listeners/SyncDepartmentListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace KeyHoang\OrgModule\Listeners;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use KeyHoang\OrgModule\Events\SyncDepartmentEvent;
+use KeyHoang\OrgModule\Services\DepartmentService;
+use Throwable;
+
+class SyncDepartmentListener implements ShouldQueue
+{
+
+    public ?string $queue = 'sync-department-listener';
+
+    /**
+     * Handle the event.
+     *
+     * @param SyncDepartmentEvent $event
+     *
+     * @return void
+     */
+    public function handle(SyncDepartmentEvent $event): void
+    {
+        $departments       = $event->data;
+        $departmentService = new DepartmentService();
+        foreach ($departments as $department) {
+            $departmentService->sync($department);
+        }
+    }
+
+    /**
+     * Handle a job failure.
+     */
+    public function failed(SyncDepartmentEvent $event, Throwable $exception): void
+    {
+        Log::error($exception->getMessage(), $event->data->toArray());
+    }
+
+}
