98c3b5be5ba7e4450cf887f84f19e04d08e912a2
namhp <haphuongnam81999@gmail.com>
2024-03-03 16:46:27 +0700

    update user no sql model

--- src/Models/UserNoSQL.php
+++ src/Models/UserNoSQL.php
@@ -3,7 +3,7 @@
 namespace KeyHoang\OrgModule\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use MongoDB\Laravel\Eloquent\Model;
 use MongoDB\Laravel\Eloquent\SoftDeletes;
 
